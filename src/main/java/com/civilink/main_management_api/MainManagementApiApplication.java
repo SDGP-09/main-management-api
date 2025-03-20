72f28fe8c64e4f040dba4e0b8be5d2aa2ebc574a
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2025-03-20 20:04:31 +0530

    - Initial commit - final build name change to main-management - server port set to 7878

---
+++ src/main/java/com/civilink/main_management_api/MainManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.civilink.main_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/civilink/main_management_api/MainManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civilink.main_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
