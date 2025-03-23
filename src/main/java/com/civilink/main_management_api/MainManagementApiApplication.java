c10f906886d791b81d35ec4051474f2402b5530e
lasithLRI <lasith.hettiarachchi.dev@gmail.com>
2025-03-23 19:52:57 +0530

    - Discovery server dependancy added and enabled

--- src/main/java/com/civilink/main_management_api/MainManagementApiApplication.java
+++ src/main/java/com/civilink/main_management_api/MainManagementApiApplication.java
@@ -2,8 +2,10 @@ package com.civilink.main_management_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MainManagementApiApplication {
 
 	public static void main(String[] args) {
